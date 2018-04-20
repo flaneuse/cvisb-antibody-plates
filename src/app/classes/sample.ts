ff0d6800295fe9c2cd1afa708541fef9cf0307ea
Laura Hughes <flaneuse@users.noreply.github.com>
2018-04-20 16:18:47 -0700

    basic plate architecture

---
+++ src/app/classes/sample.ts
@@ -0,0 +1,13 @@
+export class Sample {
+  constructor(
+    public row: string,
+    public col: string,
+    public cell: string,
+    public sample_id: string,
+    public sample_type: string,
+    public donor: string,
+    public dilution: string
+  ) {
+  }
+
+}
--- src/app/plates/plates.component.ts
+++ src/app/plates/plates.component.ts
@@ -8,9 +8,67 @@ import { Component, OnInit } from '@angular/core';
 export class PlatesComponent implements OnInit {
 
 
+  private plate_rows: string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']; // 96 well plate; row labels
+  private plate_cols: number[] = [1,2,3,4,5,6,7,8,9,10,11,12]
+  private num_plates: number;
+  private num_repl: number = 3;
+  private dilutions: number[] = [150, 1500, 3500];
+  private donors: string[] = ['donor A'];
+
+  private plate_num: number = 1;
+  private expt_id: string = 'NKD';
+  private samples: string[] = [
+    'TUL 7',
+    'TUL 1',
+    'TUL 2',
+    'TUL 3',
+    'TUL 4',
+    'TUL 5',
+    'TUL 6',
+    'TUL 8'];
+
+  private controls: string[] = [
+    'no fluorescence',
+    'HIV neg',
+    'positive control'
+  ];
+
+  private plate: Array<Object> = [];
+
+  private replicates: string[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.createReplicates(this.samples, this.controls, this.num_repl);
+  }
+
+  createReplicates(samples, controls, num_repl) {
+
+    samples = samples.sort();
+
+    samples = samples.concat(controls);
+
+
+    for (let i = 0; i < samples.length; i++) {
+      for (let j = 0; j < num_repl; j++) {
+        this.replicates.push(samples[i]);
+      }
+    }
+
+    console.log(this.replicates)
+  }
+
+  onItemDrop(e: any) {
+    // Get the dropped data here
+    // this.selectedSamples.push(e.dragData);
+    // console.log(this.droppedItems)
+    //
+    console.log('dropped')
+    console.log(e)
+    console.log(e.target)
+    //
+    // this.selectedSamples = this.droppedItems;
   }
 
 }
